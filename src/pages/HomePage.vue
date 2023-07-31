42231e8d789132d2a7e1497a676eb036db99a6dc
Connor <csully710@gmail.com>
2023-07-31 13:59:14 -0400

    added welcome page

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,4 +1,21 @@
 <template>
-  <q-page> Sup BOI </q-page>
+  <q-page class="window-width row justify-center">
+    <q-card class="q-my-xl card-style">
+      <q-card-section vertical class="text-h4" align="center">
+        Welcome to the Homepage
+      </q-card-section>
+      <q-card-section vertical class="text-body1" align="center">
+        Use the Navigation bar on the left to view all of the available tools,
+        you can logout at anytime by using the logout button in the top right.
+      </q-card-section>
+    </q-card>
+  </q-page>
 </template>
 <script></script>
+<style>
+.card-style {
+  background-color: rgb(68, 68, 68);
+  max-width: 75%;
+  height: 50%;
+}
+</style>
